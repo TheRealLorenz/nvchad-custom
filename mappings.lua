149ad01f3f74916ee870d463b69a8ad5c50e198c
TheRealLorenz <lollobellina@gmail.com>
2023-03-16 15:35:42 +0100

    Add movement in terminal mode

--- mappings.lua
+++ mappings.lua
@@ -7,4 +7,13 @@ M.nvimtree = {
   }
 }
 
+M.nvterm = {
+  t = {
+    ["<C-k>"] = { "<C-\\><C-n><C-w>k", "move up" },
+    ["<C-j>"] = { "<C-\\><C-n><C-w>j", "move down" },
+    ["<C-h>"] = { "<C-\\><C-n><C-w>h", "move left" },
+    ["<C-l>"] = { "<C-\\><C-n><C-w>l", "move right" },
+  }
+}
+
 return M
