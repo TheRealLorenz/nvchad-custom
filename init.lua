19b46b649c7ed6a4f4da4cab57a15878f2cea232
TheRealLorenz <lollobellina@gmail.com>
2023-04-03 18:31:22 +0200

    Add auto-resive autocmd

--- init.lua
+++ init.lua
@@ -1,1 +1,10 @@
+local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+autocmd("VimResized", {
+  pattern = "*",
+  command = "tabdo wincmd =",
+})
+
+-- Options
 vim.opt.listchars = { tab = "»·", eol = "↲", nbsp = "␣", trail = "·" }
